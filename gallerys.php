8ad4b308b58640c3d1a527681ede7df38f0c877f
prakashuniyal01 <uniyalprakash7@gmail.com>
2024-02-01 21:55:52 +0530

    lele lund

--- gallerys.php
+++ gallerys.php
@@ -6,35 +6,61 @@
     </h2>
   </div>
   <div class="container">
-    <div class="row">
-      <div class="col-md-4">
+  <div class="row">
+      <!-- Box 1 -->
+      <div class="col-md-6 col-lg-3 mt-4">
         <div class="img-box">
-          <img src="images/service-img/01birthday.jpg" class="img-fluid" alt="1">
+          <img  width="100%" height="300px" src="images/service-img/01birthday.jpg"  alt="" >
         </div>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-6 col-lg-3 mt-4">
         <div class="img-box">
-          <img src="images/gallery/01balloon.jpg" class="img-fluid" alt="1">
+          <img  width="100%" height="300px" src="images/service-img/01.jpeg" class="" alt="">
         </div>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-6 col-lg-3 mt-4">
         <div class="img-box">
-          <img src="images/gallery/party1.jpeg" class="img-fluid" alt="4">
+          <img  width="100%" height="300px" src="images/gallery/510aUwSpdrL._AC_UF10001000_QL80_.jpg" class="" alt="">
         </div>
       </div>
-    </div>
-  </div>
+      <div class="col-md-6 col-lg-3 mt-4">
+        <div class="img-box">
+          <img  width="100%" height="300px" src="images/gallery/bann3.jpg" class="" alt="">
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mt-4">
+        <div class="img-box">
+          <img  width="100%" height="300px" src="images/service-img/01birthday.jpg"  alt="" >
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mt-4">
+        <div class="img-box">
+          <img  width="100%" height="300px" src="images/service-img/01.jpeg" class="" alt="">
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mt-4">
+        <div class="img-box">
+          <img  width="100%" height="300px" src="images/gallery/510aUwSpdrL._AC_UF10001000_QL80_.jpg" class="" alt="">
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mt-4">
+        <div class="img-box">
+          <img  width="100%" height="300px" src="images/gallery/bann3.jpg" class="" alt="">
+        </div>
+      </div>
+</div>
 
-  <div class="container">
+
+  <div class="container mt-5">
   <div class="row">
     <div class="col-md-6">
       <div class="img-box">
-        <img src="images/gallery/3.jpg" class="img-fluid" alt="4">
+        <img width="100%" height="300px" src="images/gallery/3.jpg" class="" alt="4">
       </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-6 mt-4">
       <div class="img-box">
-        <img src="images/gallery/party4.jpeg" class="img-fluid" alt="3">
+        <img width="100%" height="300px"  src="images/gallery/party4.jpeg" class="" alt="3">
       </div>
     </div>
   </div>
